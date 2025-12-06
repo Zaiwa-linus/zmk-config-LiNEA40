a06984982e6ff63977c6dbca2f9f62e2953cc38c
Mitsuhiro <mitsuhiro.n.0919@gmail.com>
2025-12-06 23:31:57 +0900

    add

--- config/app/src/behavior_bt_layer.c
+++ config/app/src/behavior_bt_layer.c
@@ -12,41 +12,44 @@
 #include <zephyr/logging/log.h>
 #include <drivers/behavior.h>
 #include <zmk/behavior.h>
-#include <zmk/ble.h>
 #include <zmk/keymap.h>
+
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+#include <zmk/ble.h>
+#endif
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 struct behavior_bt_layer_config {
-    int layer_profile_1;
-    int layer_profile_2;
+    uint8_t layer_profile_1;
+    uint8_t layer_profile_2;
 };
 
-static int update_layers_for_profile(const struct device *dev, int profile) {
+static int update_layers_for_profile(const struct device *dev, uint8_t profile) {
     const struct behavior_bt_layer_config *config = dev->config;
 
-    int layer1 = config->layer_profile_1;
-    int layer2 = config->layer_profile_2;
+    uint8_t layer1 = config->layer_profile_1;
+    uint8_t layer2 = config->layer_profile_2;
 
     LOG_DBG("bt_layer: Updating layers for profile %d (layer1=%d, layer2=%d)",
             profile, layer1, layer2);
 
     switch (profile) {
         case 1:
-            zmk_keymap_layer_activate(layer1);
-            zmk_keymap_layer_deactivate(layer2);
+            zmk_keymap_layer_activate(layer1, false);
+            zmk_keymap_layer_deactivate(layer2, false);
             LOG_INF("bt_layer: Profile 1 - Layer %d ON, Layer %d OFF", layer1, layer2);
             break;
         case 2:
-            zmk_keymap_layer_activate(layer2);
-            zmk_keymap_layer_deactivate(layer1);
+            zmk_keymap_layer_activate(layer2, false);
+            zmk_keymap_layer_deactivate(layer1, false);
             LOG_INF("bt_layer: Profile 2 - Layer %d OFF, Layer %d ON", layer1, layer2);
             break;
         default:
-            zmk_keymap_layer_deactivate(layer1);
-            zmk_keymap_layer_deactivate(layer2);
+            zmk_keymap_layer_deactivate(layer1, false);
+            zmk_keymap_layer_deactivate(layer2, false);
             LOG_INF("bt_layer: Profile %d - Layer %d OFF, Layer %d OFF", profile, layer1, layer2);
             break;
     }
@@ -57,17 +60,18 @@ static int update_layers_for_profile(const struct device *dev, int profile) {
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
-    int profile = binding->param1;
+    uint8_t profile = binding->param1;
 
     LOG_DBG("bt_layer: Pressed with profile %d", profile);
 
+#if IS_ENABLED(CONFIG_ZMK_BLE)
     int ret = zmk_ble_prof_select(profile);
     if (ret < 0) {
         LOG_ERR("bt_layer: Failed to select BT profile %d (err %d)", profile, ret);
         return ret;
     }
-
     LOG_INF("bt_layer: Selected BT profile %d", profile);
+#endif
 
     return update_layers_for_profile(dev, profile);
 }
