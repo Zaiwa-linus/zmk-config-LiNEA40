21cb6dd4da76b1e23e67c2de745b1ab9b4151363
Mitsuhiro <mitsuhiro.n.0919@gmail.com>
2026-03-05 22:30:25 +0900

    add

--- modules/custom_battery_typist/src/behavior_battery_type.c
+++ modules/custom_battery_typist/src/behavior_battery_type.c
@@ -58,76 +58,117 @@ struct char_keycode {
     bool shift;
 };
 
-#include <dt-bindings/zmk/keys.h>
+/* HID Usage IDs (USB HID Usage Tables, Keyboard/Keypad Page 0x07) */
+#define HID_KEY_A          0x04
+#define HID_KEY_B          0x05
+#define HID_KEY_C          0x06
+#define HID_KEY_D          0x07
+#define HID_KEY_E          0x08
+#define HID_KEY_F          0x09
+#define HID_KEY_G          0x0A
+#define HID_KEY_H          0x0B
+#define HID_KEY_I          0x0C
+#define HID_KEY_J          0x0D
+#define HID_KEY_K          0x0E
+#define HID_KEY_L          0x0F
+#define HID_KEY_M          0x10
+#define HID_KEY_N          0x11
+#define HID_KEY_O          0x12
+#define HID_KEY_P          0x13
+#define HID_KEY_Q          0x14
+#define HID_KEY_R          0x15
+#define HID_KEY_S          0x16
+#define HID_KEY_T          0x17
+#define HID_KEY_U          0x18
+#define HID_KEY_V          0x19
+#define HID_KEY_W          0x1A
+#define HID_KEY_X          0x1B
+#define HID_KEY_Y          0x1C
+#define HID_KEY_Z          0x1D
+#define HID_KEY_1          0x1E
+#define HID_KEY_2          0x1F
+#define HID_KEY_3          0x20
+#define HID_KEY_4          0x21
+#define HID_KEY_5          0x22
+#define HID_KEY_6          0x23
+#define HID_KEY_7          0x24
+#define HID_KEY_8          0x25
+#define HID_KEY_9          0x26
+#define HID_KEY_0          0x27
+#define HID_KEY_SPACE      0x2C
+#define HID_KEY_MINUS      0x2D
+#define HID_KEY_SEMICOLON  0x33
+#define HID_KEY_PERIOD     0x37
+#define HID_KEY_LSHIFT     0xE1
 
 static const struct char_keycode CHAR_MAP[] = {
-    ['0'] = { .keycode = HID_USAGE_KEY_KEYBOARD_0_AND_RIGHT_PARENTHESIS, .shift = false },
-    ['1'] = { .keycode = HID_USAGE_KEY_KEYBOARD_1_AND_EXCLAMATION_POINT, .shift = false },
-    ['2'] = { .keycode = HID_USAGE_KEY_KEYBOARD_2_AND_AT, .shift = false },
-    ['3'] = { .keycode = HID_USAGE_KEY_KEYBOARD_3_AND_HASH, .shift = false },
-    ['4'] = { .keycode = HID_USAGE_KEY_KEYBOARD_4_AND_DOLLAR, .shift = false },
-    ['5'] = { .keycode = HID_USAGE_KEY_KEYBOARD_5_AND_PERCENT, .shift = false },
-    ['6'] = { .keycode = HID_USAGE_KEY_KEYBOARD_6_AND_CARET, .shift = false },
-    ['7'] = { .keycode = HID_USAGE_KEY_KEYBOARD_7_AND_AMPERSAND, .shift = false },
-    ['8'] = { .keycode = HID_USAGE_KEY_KEYBOARD_8_AND_ASTERISK, .shift = false },
-    ['9'] = { .keycode = HID_USAGE_KEY_KEYBOARD_9_AND_LEFT_PARENTHESIS, .shift = false },
-    ['A'] = { .keycode = HID_USAGE_KEY_KEYBOARD_A, .shift = true },
-    ['B'] = { .keycode = HID_USAGE_KEY_KEYBOARD_B, .shift = true },
-    ['C'] = { .keycode = HID_USAGE_KEY_KEYBOARD_C, .shift = true },
-    ['D'] = { .keycode = HID_USAGE_KEY_KEYBOARD_D, .shift = true },
-    ['E'] = { .keycode = HID_USAGE_KEY_KEYBOARD_E, .shift = true },
-    ['F'] = { .keycode = HID_USAGE_KEY_KEYBOARD_F, .shift = true },
-    ['G'] = { .keycode = HID_USAGE_KEY_KEYBOARD_G, .shift = true },
-    ['H'] = { .keycode = HID_USAGE_KEY_KEYBOARD_H, .shift = true },
-    ['I'] = { .keycode = HID_USAGE_KEY_KEYBOARD_I, .shift = true },
-    ['J'] = { .keycode = HID_USAGE_KEY_KEYBOARD_J, .shift = true },
-    ['K'] = { .keycode = HID_USAGE_KEY_KEYBOARD_K, .shift = true },
-    ['L'] = { .keycode = HID_USAGE_KEY_KEYBOARD_L, .shift = true },
-    ['M'] = { .keycode = HID_USAGE_KEY_KEYBOARD_M, .shift = true },
-    ['N'] = { .keycode = HID_USAGE_KEY_KEYBOARD_N, .shift = true },
-    ['O'] = { .keycode = HID_USAGE_KEY_KEYBOARD_O, .shift = true },
-    ['P'] = { .keycode = HID_USAGE_KEY_KEYBOARD_P, .shift = true },
-    ['Q'] = { .keycode = HID_USAGE_KEY_KEYBOARD_Q, .shift = true },
-    ['R'] = { .keycode = HID_USAGE_KEY_KEYBOARD_R, .shift = true },
-    ['S'] = { .keycode = HID_USAGE_KEY_KEYBOARD_S, .shift = true },
-    ['T'] = { .keycode = HID_USAGE_KEY_KEYBOARD_T, .shift = true },
-    ['U'] = { .keycode = HID_USAGE_KEY_KEYBOARD_U, .shift = true },
-    ['V'] = { .keycode = HID_USAGE_KEY_KEYBOARD_V, .shift = true },
-    ['W'] = { .keycode = HID_USAGE_KEY_KEYBOARD_W, .shift = true },
-    ['X'] = { .keycode = HID_USAGE_KEY_KEYBOARD_X, .shift = true },
-    ['Y'] = { .keycode = HID_USAGE_KEY_KEYBOARD_Y, .shift = true },
-    ['Z'] = { .keycode = HID_USAGE_KEY_KEYBOARD_Z, .shift = true },
-    ['a'] = { .keycode = HID_USAGE_KEY_KEYBOARD_A, .shift = false },
-    ['b'] = { .keycode = HID_USAGE_KEY_KEYBOARD_B, .shift = false },
-    ['c'] = { .keycode = HID_USAGE_KEY_KEYBOARD_C, .shift = false },
-    ['d'] = { .keycode = HID_USAGE_KEY_KEYBOARD_D, .shift = false },
-    ['e'] = { .keycode = HID_USAGE_KEY_KEYBOARD_E, .shift = false },
-    ['f'] = { .keycode = HID_USAGE_KEY_KEYBOARD_F, .shift = false },
-    ['g'] = { .keycode = HID_USAGE_KEY_KEYBOARD_G, .shift = false },
-    ['h'] = { .keycode = HID_USAGE_KEY_KEYBOARD_H, .shift = false },
-    ['i'] = { .keycode = HID_USAGE_KEY_KEYBOARD_I, .shift = false },
-    ['j'] = { .keycode = HID_USAGE_KEY_KEYBOARD_J, .shift = false },
-    ['k'] = { .keycode = HID_USAGE_KEY_KEYBOARD_K, .shift = false },
-    ['l'] = { .keycode = HID_USAGE_KEY_KEYBOARD_L, .shift = false },
-    ['m'] = { .keycode = HID_USAGE_KEY_KEYBOARD_M, .shift = false },
-    ['n'] = { .keycode = HID_USAGE_KEY_KEYBOARD_N, .shift = false },
-    ['o'] = { .keycode = HID_USAGE_KEY_KEYBOARD_O, .shift = false },
-    ['p'] = { .keycode = HID_USAGE_KEY_KEYBOARD_P, .shift = false },
-    ['q'] = { .keycode = HID_USAGE_KEY_KEYBOARD_Q, .shift = false },
-    ['r'] = { .keycode = HID_USAGE_KEY_KEYBOARD_R, .shift = false },
-    ['s'] = { .keycode = HID_USAGE_KEY_KEYBOARD_S, .shift = false },
-    ['t'] = { .keycode = HID_USAGE_KEY_KEYBOARD_T, .shift = false },
-    ['u'] = { .keycode = HID_USAGE_KEY_KEYBOARD_U, .shift = false },
-    ['v'] = { .keycode = HID_USAGE_KEY_KEYBOARD_V, .shift = false },
-    ['w'] = { .keycode = HID_USAGE_KEY_KEYBOARD_W, .shift = false },
-    ['x'] = { .keycode = HID_USAGE_KEY_KEYBOARD_X, .shift = false },
-    ['y'] = { .keycode = HID_USAGE_KEY_KEYBOARD_Y, .shift = false },
-    ['z'] = { .keycode = HID_USAGE_KEY_KEYBOARD_Z, .shift = false },
-    [' '] = { .keycode = HID_USAGE_KEY_KEYBOARD_SPACEBAR, .shift = false },
-    [':'] = { .keycode = HID_USAGE_KEY_KEYBOARD_SEMICOLON_AND_COLON, .shift = true },
-    ['%'] = { .keycode = HID_USAGE_KEY_KEYBOARD_5_AND_PERCENT, .shift = true },
-    ['-'] = { .keycode = HID_USAGE_KEY_KEYBOARD_MINUS_AND_UNDERSCORE, .shift = false },
-    ['>'] = { .keycode = HID_USAGE_KEY_KEYBOARD_PERIOD_AND_GREATER_THAN, .shift = true },
+    ['0'] = { .keycode = HID_KEY_0, .shift = false },
+    ['1'] = { .keycode = HID_KEY_1, .shift = false },
+    ['2'] = { .keycode = HID_KEY_2, .shift = false },
+    ['3'] = { .keycode = HID_KEY_3, .shift = false },
+    ['4'] = { .keycode = HID_KEY_4, .shift = false },
+    ['5'] = { .keycode = HID_KEY_5, .shift = false },
+    ['6'] = { .keycode = HID_KEY_6, .shift = false },
+    ['7'] = { .keycode = HID_KEY_7, .shift = false },
+    ['8'] = { .keycode = HID_KEY_8, .shift = false },
+    ['9'] = { .keycode = HID_KEY_9, .shift = false },
+    ['A'] = { .keycode = HID_KEY_A, .shift = true },
+    ['B'] = { .keycode = HID_KEY_B, .shift = true },
+    ['C'] = { .keycode = HID_KEY_C, .shift = true },
+    ['D'] = { .keycode = HID_KEY_D, .shift = true },
+    ['E'] = { .keycode = HID_KEY_E, .shift = true },
+    ['F'] = { .keycode = HID_KEY_F, .shift = true },
+    ['G'] = { .keycode = HID_KEY_G, .shift = true },
+    ['H'] = { .keycode = HID_KEY_H, .shift = true },
+    ['I'] = { .keycode = HID_KEY_I, .shift = true },
+    ['J'] = { .keycode = HID_KEY_J, .shift = true },
+    ['K'] = { .keycode = HID_KEY_K, .shift = true },
+    ['L'] = { .keycode = HID_KEY_L, .shift = true },
+    ['M'] = { .keycode = HID_KEY_M, .shift = true },
+    ['N'] = { .keycode = HID_KEY_N, .shift = true },
+    ['O'] = { .keycode = HID_KEY_O, .shift = true },
+    ['P'] = { .keycode = HID_KEY_P, .shift = true },
+    ['Q'] = { .keycode = HID_KEY_Q, .shift = true },
+    ['R'] = { .keycode = HID_KEY_R, .shift = true },
+    ['S'] = { .keycode = HID_KEY_S, .shift = true },
+    ['T'] = { .keycode = HID_KEY_T, .shift = true },
+    ['U'] = { .keycode = HID_KEY_U, .shift = true },
+    ['V'] = { .keycode = HID_KEY_V, .shift = true },
+    ['W'] = { .keycode = HID_KEY_W, .shift = true },
+    ['X'] = { .keycode = HID_KEY_X, .shift = true },
+    ['Y'] = { .keycode = HID_KEY_Y, .shift = true },
+    ['Z'] = { .keycode = HID_KEY_Z, .shift = true },
+    ['a'] = { .keycode = HID_KEY_A, .shift = false },
+    ['b'] = { .keycode = HID_KEY_B, .shift = false },
+    ['c'] = { .keycode = HID_KEY_C, .shift = false },
+    ['d'] = { .keycode = HID_KEY_D, .shift = false },
+    ['e'] = { .keycode = HID_KEY_E, .shift = false },
+    ['f'] = { .keycode = HID_KEY_F, .shift = false },
+    ['g'] = { .keycode = HID_KEY_G, .shift = false },
+    ['h'] = { .keycode = HID_KEY_H, .shift = false },
+    ['i'] = { .keycode = HID_KEY_I, .shift = false },
+    ['j'] = { .keycode = HID_KEY_J, .shift = false },
+    ['k'] = { .keycode = HID_KEY_K, .shift = false },
+    ['l'] = { .keycode = HID_KEY_L, .shift = false },
+    ['m'] = { .keycode = HID_KEY_M, .shift = false },
+    ['n'] = { .keycode = HID_KEY_N, .shift = false },
+    ['o'] = { .keycode = HID_KEY_O, .shift = false },
+    ['p'] = { .keycode = HID_KEY_P, .shift = false },
+    ['q'] = { .keycode = HID_KEY_Q, .shift = false },
+    ['r'] = { .keycode = HID_KEY_R, .shift = false },
+    ['s'] = { .keycode = HID_KEY_S, .shift = false },
+    ['t'] = { .keycode = HID_KEY_T, .shift = false },
+    ['u'] = { .keycode = HID_KEY_U, .shift = false },
+    ['v'] = { .keycode = HID_KEY_V, .shift = false },
+    ['w'] = { .keycode = HID_KEY_W, .shift = false },
+    ['x'] = { .keycode = HID_KEY_X, .shift = false },
+    ['y'] = { .keycode = HID_KEY_Y, .shift = false },
+    ['z'] = { .keycode = HID_KEY_Z, .shift = false },
+    [' '] = { .keycode = HID_KEY_SPACE, .shift = false },
+    [':'] = { .keycode = HID_KEY_SEMICOLON, .shift = true },   /* US: Shift+; = : */
+    ['%'] = { .keycode = HID_KEY_5, .shift = true },            /* US: Shift+5 = % */
+    ['-'] = { .keycode = HID_KEY_MINUS, .shift = false },
+    ['>'] = { .keycode = HID_KEY_PERIOD, .shift = true },       /* US: Shift+. = > */
 };
 
 #define CHAR_MAP_SIZE (sizeof(CHAR_MAP) / sizeof(CHAR_MAP[0]))
@@ -136,7 +177,7 @@ static const struct char_keycode CHAR_MAP[] = {
  * Keystroke sending helpers
  * ---------------------------------------------------------------- */
 
-static const uint32_t HID_LSHIFT = HID_USAGE_KEY_KEYBOARD_LEFT_SHIFT;
+static const uint32_t HID_LSHIFT = HID_KEY_LSHIFT;
 
 static int send_char(char c) {
     if ((uint8_t)c >= CHAR_MAP_SIZE || CHAR_MAP[(uint8_t)c].keycode == 0) {
